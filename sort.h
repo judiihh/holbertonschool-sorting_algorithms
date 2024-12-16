f5ea26f533f8976f3b0504a0001df685b45f7906
Judith <j.espinalm12@gmail.com>
2024-12-15 20:36:33 -0400

    Compiling program

--- sort.h
+++ sort.h
@@ -15,6 +15,8 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
+#include <stddef.h>
+
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
